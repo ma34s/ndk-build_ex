2afcd036aabd035cd2b7e7d133ca58fa36e80855
ma34s <ma34s.sss@gmail.com>
2019-06-28 10:31:18 +0900

    Add project files.

---
+++ ndk-build_ex/ndk-build_ex.cpp
@@ -0,0 +1,12 @@
+// ndk-build_ex.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	std::cout << "hello" << std::endl;
+	return 0;
+}
+
